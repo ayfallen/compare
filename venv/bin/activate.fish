bb8aaf61b3012e05e5611284b997bcbbb8605c1f
Yifan Ai <ayfallen@hotmail.com>
2020-02-29 01:26:48 +1100

    Fix and venv and bug

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ayfallen/Desktop/skeletoncode/Python/venv"
+set -gx VIRTUAL_ENV "/Users/ayfallen/Desktop/Python/venv"
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | tr "." "\n")[1] -lt 3
